bbe2a6ff46f1841e3f8dbc34f1c38f6c88d5f471
Cuong Nguyen <75366526+ncuong0704@users.noreply.github.com>
2025-12-25 14:58:56 +0700

    update

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -443,7 +443,7 @@ export interface ApiGuestGuest extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     name: Schema.Attribute.String & Schema.Attribute.Required;
     publishedAt: Schema.Attribute.DateTime;
-    slug: Schema.Attribute.UID<'name'>;
+    slug: Schema.Attribute.UID<'name'> & Schema.Attribute.Required;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
